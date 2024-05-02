49171c5d257a1a61d3f8fbc6ce4359cf9e63853e
Jens Henrik Vogelius <jens.vogelius@lego.com>
2024-05-02 09:53:33 +0200

    wip coyote jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,10 +3,21 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 
+@onready var coyoteTimer = $CoyoteTimer
+
+var coyote_frames = 5
+var coyote = false
+var last_floor = false
+var jumping = false
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+
+func _ready():
+	coyoteTimer.wait_time = coyote_frames / 60 # 60 fps
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -14,8 +25,10 @@ func _physics_process(delta: float) -> void:
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
 		velocity.y = JUMP_VELOCITY
+		jumping = true
+		coyote = false
 
 	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
@@ -34,6 +47,15 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("run")
 	else:
 		animated_sprite.play("jump")
+	
+	# coyote timer
+	if is_on_floor() and jumping:
+		jumping = false
+	if !is_on_floor() and last_floor and !jumping:
+		coyote = true
+		coyoteTimer.start()
+	
+	last_floor = is_on_floor()
 		
 	# Apply movement
 	if direction:
@@ -42,3 +64,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+
+
+func _on_coyote_timer_timeout():
+	coyote = false
