d089fb65c82fa27a487c4c92417250bec4f464ca
Jens Henrik Vogelius <jens.vogelius@lego.com>
2024-05-02 10:38:25 +0200

    double jump and some cleanup

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,21 +1,20 @@
 extends CharacterBody2D
 
-
 @onready var coyoteTimer = $CoyoteTimer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 @export var coyote_frames = 5
-@export var SPEED = 130.0
-@export var JUMP_VELOCITY = -300.0
+@export var speed = 130.0
+@export var jump_velocity = -300.0
 
 var coyote = false
 var last_floor = false
 var jumping = false
 
+var double_jump_available = true
+
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
 
 func _ready():
 	coyoteTimer.wait_time = coyote_frames / 60 # 60 fps
@@ -25,16 +24,16 @@ func _physics_process(delta: float) -> void:
 	velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and (is_on_floor() or coyote):
-		if (coyote):
-			print("jumped, coyote spent")
-		else:
-			print("jumped, not coyote")
-			
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and can_jump():	
+		velocity.y = jump_velocity
 		jumping = true
-		coyote = false
-
+		
+		if double_jump_available and !is_on_floor() and !coyote:
+			double_jump_available = false
+			
+		if coyote:
+			coyote = false
+			
 	# Get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
@@ -55,22 +54,24 @@ func _physics_process(delta: float) -> void:
 
 	# Apply movement
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
 	
 	# Coyote timer
 	if is_on_floor():
+		double_jump_available = true
 		jumping = false
 	if !is_on_floor() and last_floor and !jumping:
-		print("starting coyote timer, " + str(last_floor))
 		coyote = true
 		coyoteTimer.start()
 		
 	last_floor = is_on_floor()
 
+func can_jump():
+	return is_on_floor() or double_jump_available or coyote
+	
 func _on_coyote_timer_timeout():
-	print("coyote timer ended")
 	coyote = false
