c540480f62c39826954f281fb203853af48d2c01
Jens Henrik Vogelius <jenshenrik.skuldbol@gmail.com>
2024-05-09 10:50:58 +0200

    added keyboard controls to menu, centered buttons

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -1,5 +1,9 @@
 extends Control
 
+@onready var play_button: Button = $VBoxContainer/PlayButton
+
+func _ready():
+	play_button.grab_focus()
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
